d7ecce14539a6e5dda2366a662cafbe953d7a162
Lucas Henrique Silva <159385729+lucashensil@users.noreply.github.com>
2024-02-23 08:52:05 -0300

    update func danfe

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def ler_xml_DANFE(NF):
     dic_nfe = documento['nfeProc']['NFe']['infNFe']
     dic_produtos = dic_nfe['det']
     dic_dest = dic_nfe['dest']
+    dic_emit = dic_nfe['emit']
     dic_valor = dic_nfe['total']['ICMSTot']
 
     lista_produtos = []
@@ -28,43 +29,54 @@ def ler_xml_DANFE(NF):
         lista_produtos.append((nome_produto, valor_produto))
 
 
-    inf_valor_nota = dic_valor['vNF']
+    info_valor_nota = dic_valor['vNF']
     info_valor_prod = dic_valor['vProd']
     info_valor_frete = dic_valor['vFrete']
     info_valor_desc = dic_valor['vDesc']
     info_valor_totTrib = dic_valor['vTotTrib']
 
-    inf_cnpj_vendedor = dic_nfe['emit']['CNPJ']
-    inf_nome_vendedor = dic_nfe['emit']['xNome']
+    info_cnpj_vendedor = dic_emit['CNPJ']
+    info_nome_vendedor = dic_emit['xNome']
+    info_rua_vend =  dic_emit['enderEmit']['xLgr']
+    info_bairro_vend = dic_emit['enderEmit']['xBairro']
+    info_endereco_vend = info_rua_vend + ', ' + info_bairro_vend
+    info_municipio_vend = dic_emit['enderEmit']['xMun']
+    info_uf_vend = dic_emit['enderEmit']['UF']
+    info_fone_vend = dic_emit['enderEmit']['fone']
 
-    inf_cpf_comprador = dic_dest['CPF']
-    inf_nome_comprador = dic_dest['xNome']
-    inf_rua = dic_dest['enderDest']['xLgr']
-    inf_bairro = dic_dest['enderDest']['xBairro']
-    info_endereco = inf_rua + ', ' + inf_bairro
-    info_municipio = dic_dest['enderDest']['xMun']
-    info_uf = dic_dest['enderDest']['UF']
-    info_fone = dic_dest['enderDest']['fone']
-    info_email = dic_dest['email']
+    info_cpf_comprador = dic_dest['CPF']
+    info_nome_comprador = dic_dest['xNome']
+    info_rua = dic_dest['enderDest']['xLgr']
+    info_bairro = dic_dest['enderDest']['xBairro']
+    info_endereco_compr = info_rua + ', ' + info_bairro
+    info_municipio_compr = dic_dest['enderDest']['xMun']
+    info_uf_compr = dic_dest['enderDest']['UF']
+    info_fone_compr = dic_dest['enderDest']['fone']
+    info_email_compr = dic_dest['email']
 
 
     dic_respostas = {
         'valor_total_prod': [info_valor_prod],
-        'valor_total_nota': [inf_valor_nota],
+        'valor_total_nota': [info_valor_nota],
         'valor_desconto': [info_valor_desc],
         'valor_frete': [info_valor_frete],
         'valor_total_tributos': [info_valor_totTrib],
         
-        'cnpj_vendedor': [inf_cnpj_vendedor],
-        'nome_vendedor': [inf_nome_vendedor],
+        'cnpj_vendedor': [info_cnpj_vendedor],
+        'nome_vendedor': [info_nome_vendedor],
+        'endereco_vendedor': [info_endereco_vend],
+        'municipio_vendedor': [info_municipio_vend],
+        'estado_vendedor': [info_uf_vend],
+        'fone_comprador': [info_fone_vend],
 
-        'endereo_comprador': [info_endereco],
-        'municipio_comprador': [info_municipio],
-        'estado_comprador': [info_uf],
-        'cpf_comprador': [inf_cpf_comprador],
-        'nome_comprador': [inf_nome_comprador],
-        'fone_comprador': [info_fone],
-        'email_comprador': [info_email],
+        'endereo_comprador': [info_endereco_compr],
+        'municipio_comprador': [info_municipio_compr],
+        'estado_comprador': [info_uf_compr],
+        'cpf_comprador': [info_cpf_comprador],
+        'nome_comprador': [info_nome_comprador],
+        'fone_comprador': [info_fone_compr],
+        'email_comprador': [info_email_compr],
+        
         'produtos': [lista_produtos]
     }
 
@@ -83,26 +95,26 @@ def ler_xml_servico(NF):
 
     dic_nfe = documento['ConsultarNfseResposta']['ListaNfse']['CompNfse']['Nfse']['InfNfse']
 
-    inf_valor = dic_nfe['Servico']['Valores']['ValorServicos']
-    inf_servicos = dic_nfe['Servico']['Discriminacao']
+    info_valor = dic_nfe['Servico']['Valores']['ValorServicos']
+    info_servicos = dic_nfe['Servico']['Discriminacao']
 
-    inf_cnpj_vendedor = dic_nfe['PrestadorServico']['IdentificacaoPrestador']['Cnpj']
-    inf_nome_vendedor = dic_nfe['PrestadorServico']['NomeFantasia']
+    info_cnpj_vendedor = dic_nfe['PrestadorServico']['IdentificacaoPrestador']['Cnpj']
+    info_nome_vendedor = dic_nfe['PrestadorServico']['NomeFantasia']
 
     try:
-        inf_cpfCnpj_comprador = dic_nfe['TomadorServico']['IdentificacaoTomador']['CpfCnpj']['Cnpj']
+        info_cpfCnpj_comprador = dic_nfe['TomadorServico']['IdentificacaoTomador']['CpfCnpj']['Cnpj']
     except:
-        inf_cpfCnpj_comprador = dic_nfe['TomadorServico']['IdentificacaoTomador']['CpfCnpj']['Cpf']
-    inf_nome_comprador = dic_nfe['TomadorServico']['RazaoSocial']
+        info_cpfCnpj_comprador = dic_nfe['TomadorServico']['IdentificacaoTomador']['CpfCnpj']['Cpf']
+    info_nome_comprador = dic_nfe['TomadorServico']['RazaoSocial']
 
 
     dic_respostas = {
-        'valor_total': [inf_valor],
-        'cnpj_vendedor': [inf_cnpj_vendedor],
-        'nome_vendedor': [inf_nome_vendedor],
-        'CpfCnpj_comprador': [inf_cpfCnpj_comprador],
-        'nome_comprador': [inf_nome_comprador],
-        'servicos': [inf_servicos]
+        'valor_total': [info_valor],
+        'cnpj_vendedor': [info_cnpj_vendedor],
+        'nome_vendedor': [info_nome_vendedor],
+        'CpfCnpj_comprador': [info_cpfCnpj_comprador],
+        'nome_comprador': [info_nome_comprador],
+        'servicos': [info_servicos]
     }
     return dic_respostas
 
